38d6546badfc26d7fe9d03ec690d419cd06a1346
Tiago Mota <tmota.seg@gmail.com>
2026-01-21 21:52:49 -0300

    feat: casos de teste.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -8,7 +8,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
-    const longText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '.repeat(20)
+    const longText = 'Lorem ipsum dolor sit amet!'.repeat(20)
 
     cy.get('#firstName').type('Tiago')
     cy.get('#lastName').type('Mota')
@@ -19,5 +19,22 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('.success').should('be.visible')
   })
-})
 
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+     cy.get('#firstName').type('Tiago')
+    cy.get('#lastName').type('Mota')
+    cy.get('#email').type('123@hotmail,com')
+    cy.get('#phone').type('1234567890')
+    cy.get('#open-text-area').type('Test!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it.only('campo telefone continua vazio quando preenchido com valor não numérico', () => {
+    cy.get('#phone')
+      .type('abcdefghij')
+      .should('have.value', '')
+  })
+
+})

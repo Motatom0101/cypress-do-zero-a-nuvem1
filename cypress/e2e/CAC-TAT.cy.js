90d03e71407c5020a2c8875d075207025996f091
Tiago Mota <tmota.seg@gmail.com>
2026-02-24 19:56:17 -0300

    feat: Insersão de comentários.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,12 +1,16 @@
+// Suite de testes APP Central de Atendimento ao Cliente TAT
 describe('Central de Atendimento ao Cliente TAT', () => {
+  // Executa antes de cada teste
   beforeEach(() => {
     cy.visit('./src/index.html')
   })
 
+  // 1º caso teste: Verifica o título da aplicação
   it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 
+  // 2º caso teste: Preenche os campos obrigatórios e envia o formulário
   it('preenche os campos obrigatórios e envia o formulário', () => {
     const longText = 'Lorem ipsum dolor sit amet!'.repeat(20)
 
@@ -20,8 +24,9 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success').should('be.visible')
   })
 
+  // 3º caso teste: Exibe mensagem de erro email com formatação inválida
   it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
-     cy.get('#firstName').type('Tiago')
+    cy.get('#firstName').type('Tiago')
     cy.get('#lastName').type('Mota')
     cy.get('#email').type('123@hotmail,com')
     cy.get('#phone').type('1234567890')
@@ -31,10 +36,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.error').should('be.visible')
   })
 
-  it.only('campo telefone continua vazio quando preenchido com valor não numérico', () => {
+  // 4º caso teste: Exibe mensagem de erro telefone obrigatório não preenchido
+  it('campo telefone continua vazio quando preenchido com valor não numérico', () => {
     cy.get('#phone')
-      .type('abcdefghij')
-      .should('have.value', '')
+      .type('abcdefghij') // valor não numérico
+      .should('have.value', '') // verifica se o campo continua vazio, pois só aceita números
   })
 
 })
